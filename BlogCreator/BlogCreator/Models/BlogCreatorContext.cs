37cb470b745c26f4e40cbcc20ed5960d929fdcbe
elena-gertsij <gertsij.elena@yandex.ua>
2017-10-05 14:35:50 +0300

    add DbContext

---
+++ BlogCreator/BlogCreator/Models/BlogCreatorContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogCreator.Models
+{
+    public class BlogCreatorContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        public BlogCreatorContext(DbContextOptions<BlogCreatorContext> options)
+            : base(options)
+        {
+        }
+    }
+}
