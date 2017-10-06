6188f4ae11b71167e7fbaf94a4c9115f0b6a7b35
elena-gertsij <gertsij.elena@yandex.ua>
2017-10-06 18:11:55 +0300

    add connection to db

---
+++ BlogCreator/BlogCreator/Models/DbInitializer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogCreator.Models
+{
+    public class DbInitializer
+    {
+        public static void Initialize(BlogCreatorContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Users.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var users = new User[]
+            {
+            new User{ UserId =1,UserName ="Helen",Email ="helen@gmail.com",Phone="0500765616",Avatar="helen.jpg"}
+           
+            };
+            foreach (User u in users)
+            {
+                context.Users.Add(u);
+            }
+            context.SaveChanges();
+
+            var blogs = new Blog[]
+            {
+            new Blog{BlogId=1,BlogName="BlogName",BlogDescription="BlogDescription",SectionsName="SectionsName",DateCreated=DateTime.Parse("2005-09-01"),UserId=1}
+           
+            };
+            foreach (Blog b in blogs)
+            {
+                context.Blogs.Add(b);
+            }
+            context.SaveChanges();
+
+            var comments = new Comment[]
+            {
+            new Comment{ CommentId =1,CommentDescription="CommentDescription",UserName="UserName",BlogId=1}
+          
+            };
+            foreach (Comment c in comments)
+            {
+                context.Comments.Add(c);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- BlogCreator/BlogCreator/Startup.cs
+++ BlogCreator/BlogCreator/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using BlogCreator.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogCreator
 {
@@ -21,6 +23,10 @@ namespace BlogCreator
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            // добавляем контекст MobileContext в качестве сервиса в приложение
+            services.AddDbContext<BlogCreatorContext>(options =>
+                options.UseSqlServer(connection));
             services.AddMvc();
         }
 
